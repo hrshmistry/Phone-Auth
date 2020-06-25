38ffc6b9c5f7d4e25cd98ef0107e5dbbbf345c02
hrshmistry <harsh241299mistry@gmail.com>
2020-06-25 23:38:35 +0530

    Added Navigation Drawer

--- app/src/main/java/com/hrshmistry/gctask/MainActivity.kt
+++ app/src/main/java/com/hrshmistry/gctask/MainActivity.kt
@@ -2,20 +2,46 @@ package com.hrshmistry.gctask
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var mAuth: FirebaseAuth? = null
 
     private var mLogoutBtn: Button? = null
 
+    var toolbar: Toolbar? = null
+    var drawerLayout: DrawerLayout? = null
+    var navView: NavigationView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(R.layout.content_main)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+
+        drawerLayout?.addDrawerListener(toggle)
+        toggle.syncState()
+        navView!!.setNavigationItemSelectedListener(this)
 
 //      initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance()
@@ -38,10 +64,10 @@ class MainActivity : AppCompatActivity() {
         if (currentUser == null) {
             sendUserToLogin()
         }
-//        else
-//        {
-//            setContentView(R.layout.activity_main)
-//        }
+//      else
+//      {
+//         setContentView(R.layout.activity_main)
+//      }
     }
 
     private fun sendUserToLogin() {
@@ -52,4 +78,47 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_home -> {
+                Toast.makeText(
+                    this,
+                    "Home clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.nav_search -> {
+                Toast.makeText(
+                    this,
+                    "Search clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.nav_about_us -> {
+                Toast.makeText(
+                    this,
+                    "About Us clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.nav_my_profile -> {
+                Toast.makeText(
+                    this,
+                    "My Profile clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            R.id.nav_logout -> {
+                Toast.makeText(
+                    this,
+                    "Logout clicked",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+        drawerLayout?.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
+
